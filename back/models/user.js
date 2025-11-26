1801333ec029989b10c0512eb374dcee5b14afea
Steven Rivière <stevensteriviere@gmail.com>
2025-11-26 19:25:58 +0100

    update user for link with bar

--- back/models/user.js
+++ back/models/user.js
@@ -2,28 +2,31 @@ import {DataTypes} from "sequelize";
 import sequelize from "../config/database.js";
 
 const User = sequelize.define('User',{
-    name : {
-        type : DataTypes.STRING,
-        required : [true, 'veuillez entrer un prénom'],
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     email: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
-        validate: {
-        isEmail: true
+        validate: { 
+            isEmail: true 
         },
-        required : [true, 'Un mail est obligatoire'],
     },
     password: {
         type: DataTypes.STRING,
         allowNull: false,
-        required : [true, 'Le mot de passe est obligatoire'],
     },
     role: {
         type: DataTypes.ENUM("ADMIN", "GERANT", "SERVEUR", "BARMAN", "STAGIAIRE"),
         defaultValue: "SERVEUR",
-    }
+    },
+    bar_id: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+}
+
 },
 {timestamps: false});
 
