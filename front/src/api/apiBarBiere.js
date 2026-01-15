86ae257f6f1f3074530ab53f32c940fec68a5e90
Steven Rivière <stevensteriviere@gmail.com>
2026-01-15 16:29:59 +0100

    refactor: convert barbiere paiement API functions to English

--- front/src/api/apiBarBiere.js
+++ front/src/api/apiBarBiere.js
@@ -1,26 +1,26 @@
 import apiFetch from "./apiClient";
 
-export const fetchBarBieres = (bar_id) =>
+export const fetchBarBeers = (bar_id) =>
   apiFetch(`/barBiere/${bar_id}/bieres`);
 
-export const addBiereToBar = (bar_id, biere_id) =>
+export const addBeerToBar = (bar_id, biere_id) =>
   apiFetch(`/barBiere/${bar_id}/bieres/${biere_id}`, {
     method: "POST",
     body: JSON.stringify(data),
   });
 
-export const updateBiereForBar = (bar_id, biere_id, data) =>
+export const updateBeerForBar = (bar_id, biere_id, data) =>
   apiFetch(`/barBiere/${bar_id}/bieres/${biere_id}`, {
     method: "PUT",
     body: JSON.stringify(data),
   });
 
-export const deactivateBiereFromBar = (bar_id, biere_id) =>
+export const deactivateBeerFromBar = (bar_id, biere_id) =>
   apiFetch(`/barBiere/${bar_id}/bieres/${biere_id}`, {
     method: "DELETE",
   });
 
-export const restoreBiereForBar = (bar_id, biere_id) =>
+export const restoreBeerForBar = (bar_id, biere_id) =>
   apiFetch(`/barBiere/${bar_id}/bieres/${biere_id}`, {
     method: "PATCH",
   });
--- front/src/api/apiPaiement.js
+++ front/src/api/apiPaiement.js
@@ -1,27 +1,27 @@
 import apiFetch from "./apiClient";
 
-export const fetchPaiements = () => apiFetch("/paiements");
+export const fetchPayments = () => apiFetch("/paiements");
 
-export const fetchPaiement = (id) => apiFetch(`/paiements/${id}`);
+export const fetchPayment = (id) => apiFetch(`/paiements/${id}`);
 
-export const addPaiement = (paiement) => 
+export const addPayment = (paiement) => 
     apiFetch("/paiements", {
         method: "POST",
         body: JSON.stringify(paiement),
     });
 
-export const updatePaiement = (id, paiement) =>
+export const updatePayment = (id, paiement) =>
     apiFetch(`/paiements/${id}`, {
         method: "PUT",
         body: JSON.stringify(paiement),
     });
 
-export const deactivatePaiement = (id) =>
+export const deactivatePayment = (id) =>
     apiFetch(`/paiements/${id}`, {
         method: "DELETE",
     });
 
-export const enablePaiement = (id) =>
+export const enablePayment = (id) =>
     apiFetch(`/paiements/${id}`, {
         method: "PATCH",
     });
